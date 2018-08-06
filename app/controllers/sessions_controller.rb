d65ea6f286a6ed548ced1192bd1bdf0edf8075d9
goatcheese82 <nonsquishy@gmail.com>
2018-08-06 15:35:17 -0500

    added logout to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,9 @@ class SessionsController < ApplicationController
       u.image = auth["info"]["image"]
     end
 
-    binding.pry
-
     session[:user_id] = @user.id
 
-    render "welcome/home"
+    render "users/home"
   end
 
   private
