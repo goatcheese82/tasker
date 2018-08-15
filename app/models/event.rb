8ac2489a7853faf7719f4402dc38854d8c5f8832
goatcheese82 <nonsquishy@gmail.com>
2018-08-15 15:43:49 -0500

    added class model methods to event and goal

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ApplicationRecord
   has_many :event_assignments
   has_many :users, through: :event_assignments
+
+  def self.happening_today
+    where("date >=?", Time.zone.today.beginning_of_day)
+  end
+
+  def self.not_is_it_was
+    where("date <?", Time.zone.today.beginning_of_day)
+  end
 end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,11 @@
 class Goal < ApplicationRecord
   belongs_to :user
+
+  def self.due_today
+    where("deadline >=?", Time.zone.today.beginning_of_day)
+  end
+
+  def self.overdue
+    where("deadline <?", Time.zone.today.beginning_of_day)
+  end
 end
