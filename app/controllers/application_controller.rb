c6b069b2a7865cbba9b20467c31857c557488a4b
goatcheese82 <nonsquishy@gmail.com>
2018-08-17 15:52:25 -0500

    1.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+  before_action :logged_in?
+
   def list_groups
     @groups = Group.all
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def set_current_user
+    if logged_in?
+      @current_user = User.find(session[:user_id])
+    end
   end
 end
