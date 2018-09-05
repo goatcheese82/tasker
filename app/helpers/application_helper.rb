3b958b6cfa3e593b4f284605e7a3713dce118902
goatcheese82 <nonsquishy@gmail.com>
2018-09-05 17:31:32 -0500

    changed user_cards

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
   def logged_in?
     !!session[:user_id]
   end
+
+  def phone_number_link(text)
+    sets_of_numbers = text.scan(/[0-9]+/)
+    number = "+1-#{sets_of_numbers.join('-')}"
+    link_to text, "tel:#{number}"
+  end
+  
 end
