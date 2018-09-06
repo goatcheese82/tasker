8c85ac9c1fe8608bbcf89ba309049c8523046290
goatcheese82 <nonsquishy@gmail.com>
2018-09-05 21:29:21 -0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Users
-User.create(first_name: "Bob", last_name: "Bobson", email: "bob@bob.com", address: "Bob's House", phone_number: 777, admin: true, password: "hello")
-User.create(first_name: "Ben", last_name: "Smith", email: "nonsquishy@gmail.com", address: "1928 Patrician Way", phone_number: "702-325-0055", admin: true, password: "hello")
-User.create(first_name: "bill", last_name: "bill", email: "bill@bill.com", address: "Bill's House", phone_number: "702-325-0055", admin: false, password: "hello")
+#User.create(first_name: "Ben", last_name: "Smith", email: "nonsquishy@gmail.com", address: "1928 Patrician Way", phone_number: "702-325-0055", admin: true, password: "hello")
+User.create(first_name: "Shawn", last_name: "Bishop", email: "shawnbishop0908@gmail.com", address: "1622 Campbell Dr W", phone_number: "850-496-4609", admin: false, password: "hello")
+User.create(first_name: "Jacob", last_name: "Hanson", email: "jalynnhanson@gmail.com", address: "11 Elkwood Ct", phone_number: "505-573-5441", admin: false, password: "hello")
+User.create(first_name: "Sam", last_name: "Miskin", email: "jrmiskin@live.com", address: "2834 Aspen Dr", phone_number: "701-240-9086", admin: false, password: "hello")
+User.create(first_name: "Parker", last_name: "Moeller", email: "anthonykristie@gmail.com", address: "10 Windemere Court NW", phone_number: "716-570-6694", admin: false, password: "hello")
+User.create(first_name: "Ethan", last_name: "Orse", email: "wm.orse@yahoo.com", address: "653 Fairway Ave", phone_number: "850-582-0013", admin: false, password: "hello")
+User.create(first_name: "Ethan", last_name: "Alvarez", email: "alvarezboys1343@gmail.com", address: "400 Rhonda Kay Ct # 4", phone_number: "623-826-0311", admin: false, password: "hello")
 
 #Events
-Event.create(title: "First Event", summary: "A new event", owner_id: 1, date: "2018-08-17 15:30:00")
+#Event.create(title: "First Event", summary: "A new event", owner_id: 1, date: "2018-08-17 15:30:00")
 
 #Groups
 
